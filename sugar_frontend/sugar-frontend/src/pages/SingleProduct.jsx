2c82157ed4f4985a5da54ea5a0e08a504842c895
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-08-30 14:14:30 +0530

    Single Page Fully Completed with Responsivness

--- sugar_frontend/sugar-frontend/src/pages/SingleProduct.jsx
+++ sugar_frontend/sugar-frontend/src/pages/SingleProduct.jsx
@@ -2,10 +2,21 @@ import { Link, useParams } from "react-router-dom";
 import { MdArrowForwardIos } from "react-icons/md";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Stack, Box, Text } from "@chakra-ui/react";
+import {
+  Stack,
+  Box,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  extendTheme,
+} from "@chakra-ui/react";
 import { FaRupeeSign } from "react-icons/fa";
 import { GoDotFill } from "react-icons/go";
 import { star_icon, wishlistButton_icon } from "../assets/svg/icon";
+
 
 const SingleProduct = () => {
   const { id } = useParams();
@@ -45,7 +56,7 @@ const SingleProduct = () => {
 
   return (
     <section className="border-2 border-blue-300 tablet:mt-marginSectionT mobile:mt-marginSectionTMob pb-paddingSectionB font-sans">
-      <div className="flex items-center text-[14px] gap-3 py-5 ">
+      <div className="flex items-center text-[14px] gap-3 py-5 px-8 mb-4">
         <p className="text-gray-500">Home</p>
         <span>
           <MdArrowForwardIos />
@@ -55,7 +66,7 @@ const SingleProduct = () => {
 
       <Stack
         display="flex"
-        flexDirection="row"
+        flexDirection={{xl:"row", md:"row", base:"column"}}
         alignContent="center"
         justifyContent="center"
         w="90%"
@@ -65,30 +76,32 @@ const SingleProduct = () => {
         position="relative"
         className="shadow-3xl rounded-xl"
         py={4}
-        gap={7}
+        gap={{xl:"7", base:"0"}}
       >
         <Stack
-          w="40%"
+          w={{xl:"40%", md:"40%", base:"100%"}}
           direction="row"
-          flexDirection="row-reverse"
+          flexDirection={{xl:"row-reverse", md:"row-reverse", base:"column"}}
           padding={2}
-          gap={7}
-          position="sticky"
-          top="30%"
+          gap={{xl:"8%", md:"3%"}}
+          position={{xl:"sticky", md:"sticky", base:"relative"}}
+          top={{xl:"20%", md:"15%"}}
           alignSelf="flex-start"
           maxH="80vh"
           overflow="auto"
         >
-          <Box className="border-[1px] border-gray-300 w-full h-[300px] overflow-hidden p-2 rounded-lg">
-            <img className="w-full h-full object-fill" src={bigImage} alt="" />
+          <Box className="border-[1px] border-gray-300 w-full h-[350px] overflow-hidden p-2 rounded-lg">
+            <img className="w-full h-full object-fill" src={bigImage} alt="BigImage" />
           </Box>
 
-          <Box className="flex flex-col gap-3 py-8">
+          <Box className="flex tablet:flex-col mobile:flex-row gap-3 py-8">
             {images.map((image, index) => (
               <div
                 key={index}
                 className={`border-[1px] ${
-                  selectedImage === image.url ? "border-black" : "border-gray-300"
+                  selectedImage === image.url
+                    ? "border-black"
+                    : "border-gray-300"
                 } w-[80px] h-[80px] rounded-xl overflow-hidden cursor-pointer`}
                 onClick={() => handleClick(image.url)}
               >
@@ -100,18 +113,19 @@ const SingleProduct = () => {
               </div>
             ))}
           </Box>
+
+          <hr />
         </Stack>
 
         <Stack
-          w="55%"
+          w={{xl:"55%", md:"55%", base:"100%"}}
           fontSize="20px"
           direction="row"
           flexDirection="column"
           gap={10}
           py={2}
         >
-          <Text>{title}</Text>
-
+          <Text fontWeight={{base:"600"}}>{title}</Text>
           <Box className="flex flex-col gap-2">
             <div className="flex items-center laptop:text-[13px] tablet:text-[16px] text-black border-[1px] border-black w-fit rounded-md px-2 py-1 gap-2">
               <img src={star_icon} alt="star_icon" />
@@ -126,6 +140,9 @@ const SingleProduct = () => {
             </Text>
           </Box>
 
+          <Box>
+            
+          </Box>
           <Box bg="#F7F7F7" px="10" fontSize="14px" borderRadius="5px" py="3">
             <Text as="b">AVAILABLE OFFERS!</Text>
             <ul
@@ -165,12 +182,111 @@ const SingleProduct = () => {
               src={wishlistButton_icon}
               alt="wishlistButton_icon"
             />
-            <button className="bg-black px-8 laptop:py-3 rounded-md text-white font-bold w-[40%] tablet:px-0 mobile:px-0">
+            <button className="bg-black px-8 laptop:py-3 rounded-md text-white font-bold laptop:w-[40%] tablet:w-[60%] mobile:w-[100%] tablet:px-0 mobile:px-0">
               ADD TO BAG
             </button>
           </div>
+
+          //
+          <Accordion defaultIndex={[0]} allowMultiple>
+            <AccordionItem borderY="1px" borderStyle="dashed" py={2} borderColor="gray">
+              <h2>
+                <AccordionButton>
+                  <Box
+                    as="span"
+                    fontSize="14px"
+                    fontWeight="700"
+                    flex="1"
+                    textAlign="left"
+                  >
+                    DESCRIPTION
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} fontSize="14px">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem borderY="1px" borderStyle="dashed" py={2} borderColor="gray">
+              <h2>
+                <AccordionButton>
+                  <Box
+                    as="span"
+                    fontSize="14px"
+                    fontWeight="700"
+                    flex="1"
+                    textAlign="left"
+                  >
+                    INGREDIENTS
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} fontSize="14px">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem borderY="1px" borderStyle="dashed" py={2} borderColor="gray">
+              <h2>
+                <AccordionButton>
+                  <Box
+                    as="span"
+                    fontSize="14px"
+                    fontWeight="700"
+                    flex="1"
+                    textAlign="left"
+                  >
+                    FREQUENTLY ASKED QUESTIONS
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} fontSize="14px">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem borderY="1px" borderStyle="dashed" py={2} borderColor="gray">
+              <h2>
+                <AccordionButton>
+                  <Box
+                    as="span"
+                    fontSize="14px"
+                    fontWeight="700"
+                    flex="1"
+                    textAlign="left"
+                  >
+                    ADDITIONAL DETAILS
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} fontSize="14px">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+          
         </Stack>
       </Stack>
+
+
+
     </section>
   );
 };
